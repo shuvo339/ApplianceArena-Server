49c61cafc2b9196acb652f1566cf6d3671749932
shuvo339 <shubhroshafiullah@gmail.com>
2024-08-15 00:32:38 +0600

    get all products data

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const client = new MongoClient(uri, {
     try {
       // Connect the client to the server	(optional starting in v4.7)
     //   await client.connect();
+    const appliancesCollection = client.db('applianceDB').collection('products');
+
+         // products related api 
+        app.get('/products', async (req, res) => {
+        const result = await appliancesCollection.find().toArray();
+        res.send(result)
+      })
+
       // Send a ping to confirm a successful connection
     //   await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
