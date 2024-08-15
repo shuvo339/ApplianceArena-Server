81b8790fdb842d79ee3eaf3b082fabb0d6892f6a
shuvo339 <shubhroshafiullah@gmail.com>
2024-08-15 20:22:05 +0600

    cors middleware implemented

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v8mpgvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
